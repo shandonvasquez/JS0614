e490b583bbe645b00d6f567ab5ce96d2ce8761a1
Shandon Felino Vasquez Villatoro <shandon.vasquez@panamericom.io>
2024-06-17 17:46:39 -0600

    Commit on 06/14/2024

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 //IIFE
 
 (function(){
-thi
-})();
+
+})(); 
+
+function (){}()
+
+
+function onEdit(e){
+    var row = e.range.getRow();
+    var col = e.range.getColumn();
+  
+    if (col === 2 && row > 1 && e.source.getActiveSheet().getName() === "Inventario-de-Computo" ){
+        e.source.getActiveSheet().getRange(row,4).setValue(new Date());
+  
+    }
+     
+  }
